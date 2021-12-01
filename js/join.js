dc68e7cce1e138db296cef7d06ce68c6cceaf124
Manan <mananman23@gmail.com>
2021-12-01 10:06:50 +0530

    Added apply button in join.html

---
+++ js/join.js
@@ -0,0 +1,5 @@
+// Create a function join now that will redirect the user to the join page.
+let joinlink = "https://forms.gle/CcovkBRfzBK2ijnW8";
+function joinNow() {
+    window.open(joinlink)
+}
